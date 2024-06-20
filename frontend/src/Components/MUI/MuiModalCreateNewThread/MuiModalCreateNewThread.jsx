3f40f6485e8580e955f6bc2c0aa6919454905135
Abhishek kumar <aasalex21c@gmail.com>
2024-06-20 22:03:29 +0530

    working on front end, create a new post UI

--- frontend/src/Components/MUI/MuiModalCreateNewThread/MuiModalCreateNewThread.jsx
+++ frontend/src/Components/MUI/MuiModalCreateNewThread/MuiModalCreateNewThread.jsx
@@ -3,7 +3,10 @@ import Box from '@mui/material/Box';
 import Modal from '@mui/material/Modal';
 import { useSelector, useDispatch } from 'react-redux';
 import { closeMuiModalCreateNewThreadSlice, openMuiModalCreateNewThreadSlice } from '../../../Redux/Slices/muiModalCreateNewThreadSlice.mjs';
-
+import { useRef } from 'react';
+import Utils from '../../../Utils.mjs';
+import { useCallback } from 'react';
+import { useEffect } from 'react';
 
 export default function MuiModalCreateNewThread(){
 
@@ -13,7 +16,6 @@ export default function MuiModalCreateNewThread(){
   
   const handleOpen = () => dispatch(openMuiModalCreateNewThreadSlice());
   const handleClose = () => dispatch(closeMuiModalCreateNewThreadSlice());
-
   const theme = useSelector((store)=>store.theme);
 
   const style = {
@@ -29,7 +31,47 @@ export default function MuiModalCreateNewThread(){
     color: theme.brightText
   };
 
+  const refTextarea = useRef(null);
+  const refDivVerticalLine =useRef(null);
+  const refImageUploadByUser =useRef(null);
+
+  function handleTextareaChange(refTextarea,refDivVerticalLine,refImageUploadByUser ){
+    refTextarea.current.style.height = 'auto';
+    refTextarea.current.style.height = refTextarea.current.scrollHeight + "px";
+    refDivVerticalLine.current.style.height  = Number(refTextarea.current.scrollHeight + refImageUploadByUser.current.scrollHeight - 75 )+ "px";  
+     
+  }
+  const debouncedHandleTextareaChange = useCallback(
+    Utils.debouce(
+      (refTextarea,refDivVerticalLine,refImageUploadByUser)=>handleTextareaChange(refTextarea,refDivVerticalLine,refImageUploadByUser), 100
+    ), [handleTextareaChange]
+  );
+   
+
+  function handleXBtnClick(event){
+    // clear & hide image
+      refImageUploadByUser.current.src = "";
+      refImageUploadByUser.current.style.display="none";
+    // hide self
+      event.target.style.display="none";
+      debouncedHandleTextareaChange(refTextarea,refDivVerticalLine,refImageUploadByUser);
+
+  }
+  useEffect(()=>{
+    // adjust height of vertical bar between users
+    debouncedHandleTextareaChange(refTextarea,refDivVerticalLine,refImageUploadByUser);
+    setTimeout(()=>{
+
+      if(refTextarea.current){
+        refTextarea.current.focus();
+        refTextarea.current.click();
+      }
+
+    }, 2000)
+  }, []);
   
+
+
   return (
     <div>
       
@@ -47,24 +89,33 @@ export default function MuiModalCreateNewThread(){
         }}        
         
       >
-        <Box sx={style} className={`outline-none p-[.8rem] border rounded-xl  cursor-pointer hover:border-[blue-300] relative w-[35rem] min-h-[20rem] flex flex-col reltative`} >
+        <Box sx={style} className={`outline-none p-[.8rem] border rounded-xl  cursor-pointer hover:border-[blue-300] relative w-[35rem]  flex flex-col reltative`} >
           <h2 className='font-bold self-center top-[-2rem] absolute'>New thread</h2>
-          <div className='flex w-[100%] min-h-[20rem] gap-[1rem]  border-l-[.2rem] border-yellow-500 pl-[2rem]'>
-            <div className='flex flex-col items-center justify-between ml-[-4rem]'>
-              <div className='w-[4rem]  overflow-hidden pb-[1rem]' >
+          <div className="flex gap-[2rem]">
+            <div  className='flex items-center flex-col w-[10%]'>
+              <div className='w-[4rem]  overflow-hidden  ' >
                 <img src="https://res.cloudinary.com/dwlfgbmsi/image/upload/v1718603004/SharedResources/a7syt68cd0kyj3tiyhux.png" alt="user profile image" className='w-[100%] rounded-full' />
               </div>
+              <div ref={refDivVerticalLine} className='w-[.2rem] h-[2rem] bg-gray-300 my-[1rem]'>
 
-              <div className='w-[2rem] rounded-full overflow-hidden'>
-                <img src="https://res.cloudinary.com/dwlfgbmsi/image/upload/v1718603004/SharedResources/a7syt68cd0kyj3tiyhux.png" alt="user profile image-small" />
+              </div>
+              <div className='w-[2rem] overflow-hidden'>
+                <img src="https://res.cloudinary.com/dwlfgbmsi/image/upload/v1718603004/SharedResources/a7syt68cd0kyj3tiyhux.png" alt="user profile image-small" 
+                className="w-[100%] rounded-full " />
               </div>
             </div>
 
-            <div>
+            <div className='w-[90%]'>
               <h3 className='font-medium'>alex21c</h3>
-              <textarea type="text" style={{backgroundColor: theme.backgroundHover}} />
+              <textarea ref={refTextarea} onChange={()=>debouncedHandleTextareaChange(refTextarea,refDivVerticalLine,refImageUploadByUser)} type="text" style={{backgroundColor: "transparent", color: theme.primaryText}} className='w-[100%] outline-none' placeholder="Start a thread"/>
+
+              <div className='relative'>              
+                <i className="cursor-pointer absolute right-[.5rem] top-[.5rem] fa-sharp fa-regular fa-xmark text-[1rem] text-white py-[.2rem] px-[.7rem] rounded-full bg-black hover:bg-white transition hover:text-black" onClick={(event)=>{handleXBtnClick(event)}}></i>
+                <img ref={refImageUploadByUser} src="https://miro.medium.com/v2/resize:fit:1400/1*gIoRZCKodZT12rbl6Z5Lwg.png" alt="image upload by user"/>
+              </div>
             </div>
           </div>
+          
 
         </Box>
         
