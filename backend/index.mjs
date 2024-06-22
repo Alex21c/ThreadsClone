daab53fe0074698d4d856cc992f2e1f79e0cdec6
Abhishek kumar <aasalex21c@gmail.com>
2024-06-22 08:48:26 +0530

    working on cors

--- backend/index.mjs
+++ backend/index.mjs
@@ -6,6 +6,8 @@ import ThreadRouter from "./Routes/ThreadRoutes.mjs";
 import ReplyRouter from "./Routes/ReplyRoutes.mjs";
 import mongoose, { mongo } from "mongoose";
 import passport from "./Passport/passport-config.mjs";
+import cors  from 'cors';
+
 const app = new e();
 
 // connecting to db
@@ -18,19 +20,33 @@ const app = new e();
     console.log('Exiting...');
     process.exit(1);
   });
+
 // CORS
-  app.use((req, res, next) => {
-    if (req.headers.host.includes('localhost')) {
-      // console.log('its localhost');
-      res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-    } else {
-      res.header('Access-Control-Allow-Origin', 'https://threads-clone-weld-ten.vercel.app/');
-    }
-    res.header('Access-Control-Allow-Headers', 'Authorization, Content-Type');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');    
+  // app.use((req, res, next) => {
+  //   if (req.headers.host.includes('localhost')) {
+  //     // console.log('its localhost');
+  //     res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  //   } else {
+  //     res.header('Access-Control-Allow-Origin', 'https://threads-clone-weld-ten.vercel.app/');
+  //   }
+  //   res.header('Access-Control-Allow-Headers', 'Authorization, Content-Type');
+  //   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');    
     
-    next();
-  });
+  //   next();
+  // });
+  const corsOptions = {
+    origin: (origin, callback) => {
+      if (!origin || origin.includes('localhost:3000') || origin.includes('https://threads-clone-weld-ten.vercel.app')) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    allowedHeaders: ['Authorization', 'Content-Type'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    optionsSuccessStatus: 200,
+  };
+  app.use(cors(corsOptions));
 
 
 // allowing json to be parsed in req.body
